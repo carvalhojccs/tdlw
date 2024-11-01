6d38f4e41059af12bac7729a791ebadaa570b15e
carvahojccs <jonne.cley@gmail.com>
2024-11-01 01:21:37 +0000

    refactor: configure observed by PlanObserver

--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use App\Observers\PlanObserver;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+#[ObservedBy(PlanObserver::class)]
 class Plan extends Model
 {
     /** @use HasFactory<\Database\Factories\PlanFactory> */

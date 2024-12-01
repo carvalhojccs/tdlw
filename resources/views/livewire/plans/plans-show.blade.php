1ed97fa4ca4a436c7896b51ea7d673ec6598a39e
carvahojccs <jonne.cley@gmail.com>
2024-12-01 15:01:22 +0000

    refactor: add diff fort time to update record

--- resources/views/livewire/plans/plans-show.blade.php
+++ resources/views/livewire/plans/plans-show.blade.php
@@ -1,6 +1,6 @@
 <x-slot name="header">
     <x-breadcrumb.breadcrumb>
-        <<x-breadcrumb.item :route="route('plans.index')">{{ __('Plans') }}</x-breadcrumb.item>
+        <x-breadcrumb.item :route="route('plans.index')">{{ __('Plans') }}</x-breadcrumb.item>
             <x-breadcrumb.active>{{ __('Show') }}</x-breadcrumb.active>
     </x-breadcrumb.breadcrumb>
 </x-slot>
@@ -51,7 +51,7 @@
                 <div class="relative z-0 w-full mb-5 group">
                     <span
                         class="block py-2.5 px-0 w-full text-sm text-gray-900 border-0 border-b-2 focus:outline-none focus:border-blue-600">
-                        {{ $plan->updated_at->format('d/m/yy') }}
+                        {{ $plan->updated_at->format('d/m/yy') }} | {{ $plan->created_at->diff($plan->updated_at) }}
                     </span>
                     <label
                         class="absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:text-blue-600">

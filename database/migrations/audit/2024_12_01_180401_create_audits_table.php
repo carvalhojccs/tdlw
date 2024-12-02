6da2edb6429838e1d7b52c3927fdab8fcbcd2a8f
carvahojccs <jonne.cley@gmail.com>
2024-12-02 02:02:34 +0000

    feature: migration audit created

---
+++ database/migrations/audit/2024_12_01_180401_create_audits_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    protected $connection = 'audit';
+
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('audits', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable();
+            $table->timestamp('when');
+            $table->string('event');
+            $table->jsonb('details')->nullable();
+            $table->ipAddress('ip')->nullable();
+            $table->morphs('auditable');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('audits');
+    }
+};

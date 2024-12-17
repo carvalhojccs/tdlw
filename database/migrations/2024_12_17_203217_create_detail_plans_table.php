1957649f2d4fb3581e9e3483355f03d522495fc4
carvahojccs <jonne.cley@gmail.com>
2024-12-17 20:59:04 +0000

    refactor: create fields to entity

--- database/migrations/2024_12_17_203217_create_detail_plans_table.php
+++ database/migrations/2024_12_17_203217_create_detail_plans_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Plan;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,8 @@ return new class extends Migration
     {
         Schema::create('detail_plans', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(Plan::class);
+            $table->string('name');
             $table->timestamps();
         });
     }

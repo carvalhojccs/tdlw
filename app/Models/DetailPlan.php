86cdad8b1e608d32110fbdec7df03edf76f2d085
carvahojccs <jonne.cley@gmail.com>
2024-12-17 20:57:03 +0000

    refactor: add guarded and relationship to plan

--- app/Models/DetailPlan.php
+++ app/Models/DetailPlan.php
@@ -4,9 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class DetailPlan extends Model
 {
     /** @use HasFactory<\Database\Factories\DetailPlanFactory> */
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function plan(): BelongsTo
+    {
+        return $this->belongsTo(Plan::class);
+    }
+
 }
